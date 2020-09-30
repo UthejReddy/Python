1c36cc3e51cef348f7879be795fed4eb875a64d8
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-09-30 18:06:48 +0530

    Add files via upload

---
+++ if_statements/elif_statement.py
@@ -0,0 +1,10 @@
+
+name=input("enter name:")
+if name=="kiran":
+    print("entered name is:",name)
+elif name=="sham":
+    print("entered name is :",name)
+elif name=="pawan":
+    print("entered name is :",name)
+else:
+    print("name is invalid:")
