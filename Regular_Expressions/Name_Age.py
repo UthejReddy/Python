4275dd4660ee5decf2b252cf37bd76d8bf82c3f8
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-10-09 11:06:15 +0530

    Add files via upload

---
+++ Regular_Expressions/Name_Age.py
@@ -0,0 +1,11 @@
+import re
+
+nameage = "Janice is 22 and Tom is 34 Amit is 23 Sham is 32 Shubham is 25"
+
+ages = re.findall(r'\d{1,3}',nameage)
+names = re.findall(r'[A-Z][a-z]*',nameage)
+
+print(ages)
+print(names)
+
+
