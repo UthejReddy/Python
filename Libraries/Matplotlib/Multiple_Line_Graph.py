6a1816600d3abcd596279c3d58e3f603b6b5e42e
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-10-17 09:59:28 +0530

    Add files via upload

---
+++ Libraries/Matplotlib/Multiple_Line_Graph.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+#multiple line graph
+from matplotlib import style
+
+style.use('ggplot')
+
+x=[4,5,8,9]
+y=[8,4,6,7]
+
+x2=[7,9,4,6]
+y2=[4,6,4,8]
+
+plt.plot(x,y,'g',label='line one',linewidth=5)
+plt.plot(x2,y2,'c',label='line two',linewidth=5)
+
+plt.title('Epic info')
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
+
+plt.legend()
+
+plt.grid(True,color='k')
+
+plt.show()
