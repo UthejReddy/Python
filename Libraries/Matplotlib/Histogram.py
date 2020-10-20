47b453d9c261cd85148728ce469c008ba263f1e1
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-10-20 11:33:45 +0530

    Update Histogram.py

--- Libraries/Matplotlib/Histogram.py
+++ Libraries/Matplotlib/Histogram.py
@@ -1,6 +1,9 @@
 
 ##Histogram
+##import libraries
 from matplotlib import pyplot as plt
+
+##List of integers
 ages=[10,21,54,84,20,14,56,20,32,65,89,45,18,54,45,97]
 bins=[0,10,20,30,40,50,60,70,80,90,100]
 
