aef31692f8f75930b79679ea0e9987260ef6b893
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-10-22 12:06:35 +0530

    Update Phone_number.py

--- Regular_Expressions/Phone_number.py
+++ Regular_Expressions/Phone_number.py
@@ -1,7 +1,11 @@
+#regular expression program to detect the phone number
+
+#import regular expression library
 import re
 
 phone="457-154-6566"
 
+# if-else condition
 if re.search("\d{3}-\d{3}-\d{4}",phone):
     print("its a valid number")
 else:
