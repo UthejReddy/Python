67ca28063d9aa156ce54a23fe1fe5ca10e5002c4
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-10-25 13:28:44 +0530

    Add files via upload

---
+++ Python_Basics_Codes/Create_newFeatures.py
@@ -0,0 +1,10 @@
+
+## Code to create new features
+from sklearn.preprocessing import LabelEncoder
+le_company = LabelEncoder()
+le_job = LabelEncoder()
+le_degree = LabelEncoder()
+
+inputs['company_n'] = le_company.fit_transform(inputs['company'])
+inputs['job_n'] = le_job.fit_transform(inputs['job'])
+inputs['degree_n'] = le_degree.fit_transform(inputs['degree'])
