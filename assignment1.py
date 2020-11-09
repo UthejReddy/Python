ba533b761a509b7b1712da00ca48d1a004dadeca
UthejReddy <43196841+UthejReddy@users.noreply.github.com>
2020-11-09 10:05:10 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+#create an object
 elements=input("enter numbers :")
 list=elements.split(",")
 tuple=(*list,)
@@ -5,11 +6,12 @@ print("list:",list)
 print("tuple:",tuple)
 
 
-
+#create a list
 color_list=["red","green","white","black"]
 print(color_list[0],color_list[3])
 
 
+#import datetime library
 import datetime
 date1=2014,7,11
 date2=2014,7,2
@@ -21,6 +23,7 @@ count=list1.count(number)
 count
 
 
+#create dictionary
 dic1={1:10,2:20}
 dic2={3:30,4:40}
 dic3={5:50,6:60}
@@ -28,6 +31,7 @@ result={dic1+dic2+dic3}
 result
 
 
+#import library pandas
 import pandas as pd
 ser1=pd.Series([2,4,6,8,10])
 ser2=pd.Series([1,3,5,7,9])
